227af48463da37b20052debdc427ed46d1197666
Ajinkya Palaskar <ajinkya@geekyants.com>
2022-11-30 14:17:21 +0530

    Update: Add favicon and update title

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import Head from "next/head";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -8,6 +9,23 @@ type LayoutProps = {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <div>
+      <Head>
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+
+        <meta
+          name="og:title"
+          content="Next.js Ecommerce | A simple ecommerce store made with Next.js and Redux"
+        />
+
+        <link
+          rel="icon"
+          href="https://upload.wikimedia.org/wikipedia/commons/7/76/Louis_Vuitton_logo_and_wordmark.svg"
+        />
+      </Head>
+
       <Navbar />
 
       <main>{children}</main>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           ></link>
         </Head>
+
         <body>
           <Main />
           <NextScript />
